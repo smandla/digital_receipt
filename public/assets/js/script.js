4b465b58035dc87146b8c1711dfa85896a69430d
smandla <35283559+smandla@users.noreply.github.com>
2022-07-26 01:14:30 -0700

    create some html for 2 pages and make sure drag/drop works

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,63 @@
 const buttonEl = $("#submit_btn");
 const fileEl = $("#file");
 
+const dropZoneInputEl = document.querySelector(".drop-zone__input");
+const dropZoneElement = dropZoneInputEl.closest(".drop-zone");
+dropZoneElement.addEventListener("click", (e) => {
+  console.log("clicked");
+  dropZoneInputEl.click();
+});
+dropZoneInputEl.addEventListener("change", (e) => {
+  // fileEl[0].files[0];
+  // console.log(fileEl[0].files[0]);
+  console.log(dropZoneInputEl.files[0]);
+  if (dropZoneInputEl.files.length) {
+    updateThmbNl(dropZoneInputEl.files[0]);
+  }
+});
+dropZoneElement.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZoneElement.classList.add(".drop-zone--over");
+});
+["dragleave", "dragend"].forEach((type) => {
+  dropZoneElement.addEventListener(type, (e) => {
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
+});
+dropZoneElement.addEventListener("drop", (e) => {
+  e.preventDefault();
+
+  if (e.dataTransfer.files.length) {
+    dropZoneInputEl.files = e.dataTransfer.files;
+    updateThmbNl(e.dataTransfer.files[0]);
+  }
+
+  dropZoneElement.classList.remove("drop-zone--over");
+});
+
+function updateThmbNl(file) {
+  let thmbnlEl = dropZoneElement.querySelector(".drop-zone__thumb");
+
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+  if (!thmbnlEl) {
+    thmbnlEl = document.createElement("div");
+    thmbnlEl.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thmbnlEl);
+  }
+  thmbnlEl.dataset.label = file.name;
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thmbnlEl.style.backgroundImage = `url("${reader.result}")`;
+    };
+  } else {
+    thmbnlEl.style.backgroundImage = null;
+  }
+}
+
 buttonEl.on("click", (e) => {
   e.preventDefault();
   const fileVal = fileEl[0].files[0];
@@ -8,9 +65,12 @@ buttonEl.on("click", (e) => {
   let newReceipt = new FormData();
   newReceipt.append("file", fileVal);
   console.log(newReceipt);
-  saveReceipt(newReceipt).then(() => {
-    console.log("done");
-  });
+
+  document.location.replace(`./saveReceipt`);
+  if (window.location.pathname === "/saveReceipt") {
+    console.log();
+    // saveReceipt(newReceipt);
+  }
 });
 const saveReceipt = (receipt) => {
   console.log(receipt);
